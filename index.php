7ef9c57ca37bef2e40833002a02d6c2645c18751
nruiz64 <ruiz.nicolas54@gmail.com>
2017-01-12 22:52:11 +0100

    List all user of the server

--- index.php
+++ index.php
@@ -10,7 +10,20 @@
 </head>
 
 <body>
-Hello World
+<div class="container">
+<?php
+   $output = array();
+   $command = 'cat /etc/passwd | grep "/home" |cut -d: -f1';
+   //$command = 'cat /etc/passwd | grep "/home"';
+   echo 'running the command: <b>'.$command."</b><br />";
+   $line = exec($command, $output);
+
+   $line = implode("<br />\n", $output);
+   //$allLine = explode($line,"\n");
+   //echo $allLine[0];
+   echo $line ;
+?>
+</div>
 </body>
 
 </html>
