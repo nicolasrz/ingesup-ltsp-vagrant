1039f3b632f99d471083496384c34f35cf39f368
nruiz64 <ruiz.nicolas54@gmail.com>
2017-01-12 21:30:55 +0100

    Hello world

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+<title>Title of the document</title>
+</head>
+
+<body>
+Hello World
+</body>
+
+</html>
+
